326059642a6ae395f273c281c15ee9c7a41a9517
Keith Chan <kg6kzw@gmail.com>
2017-08-08 22:51:06 -0700

    v4.0

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(function(err, req, res, next) {
 var ipAddr = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 app.listen(port, ipAddr, function(){
-	console.log(`App listening on port ${port}`);
+	console.log('App listening on port ' + port);
 });	// listen to port 8080
 
 module.exports = app;
